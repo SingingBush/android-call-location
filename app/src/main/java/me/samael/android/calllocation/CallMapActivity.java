a3a05b80c7b1fb927739a78527e6f2c713e4e602
Samael Bate <samael.bate@gmail.com>
2015-01-06 22:42:41 +0000

    starting work on updating to GoogleMaps API 2 - wip
    git-svn-id: https://android-call-location.googlecode.com/svn/trunk@29 cc469ea4-53a4-efa6-82a8-9ac84eac9d13

--- app/src/main/java/me/samael/android/calllocation/CallMapActivity.java
+++ app/src/main/java/me/samael/android/calllocation/CallMapActivity.java
@@ -8,14 +8,20 @@ import me.samael.android.calllocation.data.SharedPrefs;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.app.Activity;
+import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.SQLException;
@@ -27,79 +33,59 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import android.support.v4.app.Fragment;
+public class CallMapActivity extends Activity implements OnMapReadyCallback {
 
-public class CallMapActivity extends Fragment {
 	private static final String TAG = CallMapActivity.class.getSimpleName();
 
-	private MapView mapView;
-	private GoogleMap map;
-	Long callId;
+    //private GoogleMap map;
+    Long callId;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.callmap, container, false);
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
 
-        if (v != null) {
-            mapView = (MapView) v.findViewById(R.id.map_view);
-        } else {
-            Log.e(TAG, "MapView was null");
-        }
-        mapView.onCreate(savedInstanceState);
-
-        // Gets to GoogleMap from the MapView and does initialization stuff
-        map = mapView.getMap();
-        map.getUiSettings().setMyLocationButtonEnabled(false);
         map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
 
-        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
-        try {
-            MapsInitializer.initialize(this.getActivity());
-        } catch (GooglePlayServicesNotAvailableException e) {
-            e.printStackTrace();
-        }
-
-        // Updates the location and zoom of the MapView
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
-        map.animateCamera(cameraUpdate);
-
-        return v;
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
     }
 
-    @Override
-    public void onResume() {
-        mapView.onResume();
-        super.onResume();
-    }
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.callmap);
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
+//        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map_view)).getMap();
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map_view);
+        mapFragment.getMapAsync(this);
 
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
+        // a little something for debugging::
+//        if (map!=null) {
+//            Marker hamburg = map.addMarker(new MarkerOptions().position(new LatLng(53.558, 9.927))
+//                    .title("Hamburg"));
+//            Marker kiel = map.addMarker(new MarkerOptions()
+//                    .position(new LatLng(53.551, 9.993))
+//                    .title("Kiel")
+//                    .snippet("Kiel is cool")
+//                    .icon(BitmapDescriptorFactory
+//                            .fromResource(R.drawable.map_marker)));
+//    }
 
-//	@Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.callmap);
-//
 //        mapView = (MapView) findViewById(R.id.mapview);
 //        mapView.setBuiltInZoomControls(true);
 //
 //        mapView = (MapView)findViewById(R.id.mapview);
 //
 //        mapController = mapView.getController();
-//
-//        callId = (savedInstanceState == null) ? null : (Long) savedInstanceState.getSerializable(CallDbAdapter.KEY_ID);
-//        Bundle extras = getIntent().getExtras();
-//        callId = extras != null ? extras.getLong(CallDbAdapter.KEY_ID) : null;
-//	}
+
+            callId = (savedInstanceState == null) ? null : (Long) savedInstanceState.getSerializable(CallDbAdapter.KEY_ID);
+            Bundle extras = getIntent().getExtras();
+            callId = extras != null ? extras.getLong(CallDbAdapter.KEY_ID) : null;
+	}
 	
 //	@Override
 //    protected void onStart() {
@@ -147,5 +133,61 @@ public class CallMapActivity extends Fragment {
 //		// TODO Auto-generated method stub
 //		return false;
 //	}
+
+    // not sure if fragment needed, check docs:
+    // https://developers.google.com/maps/documentation/android/
+    public static class CallMap extends Fragment {
+
+        private MapView mapView;
+        private GoogleMap map;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.callmap, container, false);
+
+            if (v != null) {
+                mapView = (MapView) v.findViewById(R.id.map_view);
+            } else {
+                Log.e(TAG, "MapView was null");
+            }
+            mapView.onCreate(savedInstanceState);
+
+            // Gets to GoogleMap from the MapView and does initialization stuff
+            map = mapView.getMap();
+            map.getUiSettings().setMyLocationButtonEnabled(false);
+            map.setMyLocationEnabled(true);
+
+            // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+//        try {
+            MapsInitializer.initialize(getActivity());
+//        } catch (GooglePlayServicesNotAvailableException e) {
+//            e.printStackTrace();
+//        }
+
+            // Updates the location and zoom of the MapView
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+            map.animateCamera(cameraUpdate);
+
+            return v;
+        }
+
+        @Override
+        public void onResume() {
+            mapView.onResume();
+            super.onResume();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            mapView.onDestroy();
+        }
+
+        @Override
+        public void onLowMemory() {
+            super.onLowMemory();
+            mapView.onLowMemory();
+        }
+    }
 
 }
